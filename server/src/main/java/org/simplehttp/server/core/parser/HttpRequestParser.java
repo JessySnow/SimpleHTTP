9aab1d1999a56366ae4507deb81be600361b87f5
JessySnow <jessysnow12345@gmail.com>
2022-11-22 15:24:49 +0800

    Performance

--- server/src/main/java/org/simplehttp/server/core/parser/HttpRequestParser.java
+++ server/src/main/java/org/simplehttp/server/core/parser/HttpRequestParser.java
@@ -8,7 +8,6 @@ import org.simplehttp.server.enums.RequestMethod;
 import org.simplehttp.server.pojo.protocol.HttpBody;
 import org.simplehttp.server.pojo.protocol.HttpHeader;
 import org.simplehttp.server.pojo.protocol.HttpRequest;
-import org.simplehttp.server.pojo.protocol.URLWrapper;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -16,6 +15,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.Optional;
 
@@ -27,6 +27,7 @@ public class HttpRequestParser {
     private static final byte NEWLINE = '\n';
     private static final byte ENTER = '\r';
     private static final byte SPACE = ' ';
+    private static final byte COLON = ':';
 
     public HttpRequest parse(BaseServerContext context, InputStream inputStream) throws IOException, RuntimeException {
         HttpRequest request = new HttpRequest();
@@ -36,12 +37,12 @@ public class HttpRequestParser {
         byte pre = -1;
         byte cur;
 
-        // 预分配缓冲区，解析 HTTP 请求头
+        // 预分配缓冲区，解析 HTTP 请求头，单个键/请求路径长度最大限制 512 字节
         ByteBuffer buffer = ByteBuffer.allocate(512);
-        StringBuilder temp = new StringBuilder();
-        // 处理首行
+        // 处理请求头部的首行
         String method = null;
         String queryPath = null;
+        String protocolVersion = null;
         while ((cur = (byte) inputStream.read()) != NEWLINE){
             if(SPACE == cur){
                 buffer.flip();
@@ -51,6 +52,8 @@ public class HttpRequestParser {
                     method = new String(bytes);
                 }else if(queryPath == null){
                     queryPath = new String(bytes);
+                }else{
+                    protocolVersion = new String(bytes);
                 }
                 buffer.clear();
             }
@@ -58,16 +61,15 @@ public class HttpRequestParser {
                 buffer.put(cur);
             }
         }
+        buffer.clear();
 
-        //兼容单元测试和API测试 TODO release 移除
-        if(!queryPath.startsWith("http")){
-            String protocol = context.getServer().protocol;
-            String host = context.getServer().getHostAlias();
-            String contextPath = context.getServer().getContextPath();
-            int port = context.getServer().getPort();
-            queryPath = protocol + "://" + host + ":" + port + queryPath;
-        }
+        //  TODO Fix
+        String protocol = context.getServer().protocol;
+        String host = context.getServer().getHostAlias();
+        int port = context.getServer().getPort();
+        queryPath = protocol + "://" + host + ":" + port + queryPath;
 
+        // 请求方法处理，如果遇到不支持的请求方法，快速失败
         try{
             RequestMethod m = Enum.valueOf(RequestMethod.class, method);
             // 当是一个 POST 请求时，才构造 HTTP 请求体对象
@@ -78,37 +80,41 @@ public class HttpRequestParser {
             log.info("不支持的 HTTP 请求方法: " + method);
             throw new RuntimeException("不支持的 HTTP 请求方法: " + method);
         }
-        URLWrapper urlWrapper = context.getUrlParser().parse(context.server, queryPath);
-        request.setUrlWrapper(urlWrapper);
-        temp.delete(0,temp.length());
+//        URLWrapper urlWrapper = context.getUrlParser().parse(context.server, queryPath);
+//        request.setUrlWrapper(urlWrapper);
 
         // 处理头部
+        String key = null, value;
         while ((cur = (byte) inputStream.read()) != -1){
-            if(cur == NEWLINE || cur == ENTER){
+            // 键值对的冒号分割
+            if(cur == COLON){
+                buffer.flip();
+                byte[] bytes = new byte[buffer.limit()];
+                buffer.get(bytes);
+                key = new String(bytes);
+                buffer.clear();
+            } else if(cur == NEWLINE || cur == ENTER){
                 // 普通的换行，pre = '/r'
                 if(pre == ENTER){
-                    String line = temp.toString();
-                    String[] param = line.split(":");
-                    if(param.length < 2){
-                        throw new RuntimeException("损坏的 HTTP 头部");
-                    }
-                    header.addHeaderPair(param[0], param[1]);
-                    temp.delete(0, temp.length());
-                }
-                // 到达了分界处，跳出循环
-                else if(pre == NEWLINE){
+                    buffer.flip();
+                    byte[] bytes = new byte[buffer.limit()];
+                    buffer.get(bytes);
+                    value = new String(bytes);
+                    buffer.clear();
+                    header.addHeaderPair(key, value);
+                } else if(pre == NEWLINE){
+                    // 头、体 分界处，连续的两个换行符，直接退出循环
                     break;
                 }
             }else{
-                temp.append((char) cur);
+                buffer.put(cur);
             }
             pre = cur;
         }
         request.setHeader(header);
-        temp.delete(0,temp.length());
+        buffer.clear();
 
-
-
+        // 请求体处理
         if(body != null){
             // 如果不能解析头部的媒体类型，使用默认的二进制类型进行处理
             String mimeType = Optional
@@ -116,6 +122,13 @@ public class HttpRequestParser {
                             .getHeader()
                             .getHeaderValue(FixedHttpHeader.CONTENT_TYPE.key))
                     .orElse(MIME.BINARY.value);
+            String[] split = mimeType.split(";");
+            Charset charset = StandardCharsets.UTF_8;
+            if(split.length > 1){
+                mimeType = split[0];
+                charset = Charset.forName(split[1]);
+            }
+
             MIME acceptableType;
             try {
                 acceptableType = Enum.valueOf(MIME.class, mimeType.replace("/","_").toUpperCase().trim());
@@ -123,13 +136,10 @@ public class HttpRequestParser {
                 throw new RuntimeException("无法接受的媒体类型");
             }
 
-            // 字符类型统一用 UTF-8 编码读取 TODO 支持其他的编码方式
             switch (acceptableType){
-                // 如果提交的是一个纯文本信息，说明在协议层面不存在键值对关系，直接将文本读取到 String 中，BodyEntry 的键设置成媒体类型
-                // 如果需要文本信息中的键值对关系，需要在 Handler 中自己进行处理
                 case TEXT_HTML, TEXT_PLAIN -> {
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream,StandardCharsets.UTF_8));
-                    // 如果是 POST 请求，分配的 Body 空间是 8MB
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream,charset));
+                    // 纯文本的提交不要超过 8MB，超过这个大小，使用 Multipart 格式提交
                     CharBuffer charBuffer = CharBuffer.allocate(1024*8192);
                     reader.read(charBuffer);
                     charBuffer.flip();
